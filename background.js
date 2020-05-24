6a99f83559c0ac9eab151a55b2d46aa963115658
Mojtaba Javan <javan.mojtaba@gmail.com>
2020-05-25 03:19:47 +0430

    Add svg logo

--- background.js
+++ background.js
@@ -1,34 +1,62 @@
 let storage = chrome.storage.sync;
-let rules = [{
-  urlPattern: "https://parscoders.com/project/only-available/*",
-  elementSelector: ".todo-tasklist-item",
-  targetSelector: ".todo-tasklist-item-title",
-  contentPatterns: [
-    "ترجمه",
-    "مقاله",
-    "متلب",
-    "پایان نامه", "پایان‌نامه",
-    "پروژه خصوصی",
-    "موشن",
-    "انیمیشن",
-    "وردپرس",
-    "wordpress",
-    "تایپ",
-    "اندروید",
-    "پیج اینستاگرام",
-    "ربات اینستاگرام",
-    "لاراول",
-    "laravel"
-  ],
-}];
+let rules = [
+  {
+    urlPattern: "https://www.freecodecamp.org/news/?",
+    elementSelector: "article.post",
+    elementPatterns: [
+      {
+        targetInnerElementSelector: ".post-card-title a",
+        pattern:
+          "\\b(oracle|docker|scrum|hugo|azure|aws|webpack|flutter|reactjs|firebase|rails|macos|python|wordpress|java|pytorch|react|redux|xcode|mac|angular|php|yii2|laravel|django|gatsby)\\b",
+      },
+    ],
+    isEnabled: true,
+  },
+  {
+    urlPattern: "https://dev.to/?",
+    elementSelector: ".single-article",
+    elementPatterns: [
+      {
+        targetInnerElementSelector: "a.index-article-link",
+        pattern:
+          "\\b(oracle|docker|scrum|hugo|azure|aws|webpack|flutter|reactjs|firebase|rails|macos|python|wordpress|java|pytorch|react|redux|xcode|mac|angular|php|yii2|laravel|django|gatsby)\\b",
+      },
+    ],
+    isEnabled: true,
+  },
+  {
+    urlPattern: "https://dzone.com/list/?",
+    elementSelector: "div.article-content",
+    elementPatterns: [
+      {
+        targetInnerElementSelector: ".article-title",
+        pattern:
+          "\\b(oracle|docker|scrum|hugo|azure|aws|webpack|flutter|reactjs|firebase|rails|macos|python|wordpress|java|pytorch|react|redux|xcode|mac|angular|php|yii2|laravel|django|gatsby)\\b",
+      },
+    ],
+    isEnabled: true,
+  },
+  {
+    urlPattern: "https://hashnode.com/stories/?",
+    elementSelector: "div.preview-card",
+    elementPatterns: [
+      {
+        targetInnerElementSelector: "h1",
+        pattern:
+          "\\b(oracle|docker|scrum|hugo|azure|aws|webpack|flutter|reactjs|firebase|rails|macos|python|wordpress|java|pytorch|react|redux|xcode|mac|angular|php|yii2|laravel|django|gatsby)\\b",
+      },
+    ],
+    isEnabled: true,
+  },
+];
 
-chrome.runtime.onInstalled.addListener(function() {
-  storage.set({rules}, function() {
-    console.log("Hidem installed successfully. Time to save time!");
+chrome.runtime.onInstalled.addListener(function () {
+  storage.set({ rules }, function () {
+    console.log("Hidem is installed successfully. Time to save time!");
   });
 });
 
-storage.get(['rules'], function(result) {
+storage.get(["rules"], function (result) {
   if (result.rules !== undefined && result.rules !== null) {
     rules = result.rules;
     console.log(rules);
@@ -36,8 +64,8 @@ storage.get(['rules'], function(result) {
   }
 });
 
-chrome.tabs.onActivated.addListener(function(info) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.onActivated.addListener(function (info) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     /*chrome.tabs.sendMessage(tabs[0].id, {request: "hide-elements", elements: elements.filter((value) => {
       let regex = new RegExp(value.urlPattern);
       return regex.test(tabs[0].url);
@@ -46,21 +74,33 @@ chrome.tabs.onActivated.addListener(function(info) {
   });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, change, tab){
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
   //markTab(tab.url, tabId);
 });
 
-chrome.runtime.onMessage.addListener(function messageReceived(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function messageReceived(
+  message,
+  sender,
+  sendResponse
+) {
   if (message.request != undefined) {
     if (message.request == "get-rules") {
-      sendResponse(rules.filter((value) => {
-        let regex = new RegExp(value.urlPattern);
-        console.log(sender);
-        return regex.test(sender.tab.url);
-      }));
+      sendResponse(
+        rules.filter((value) => {
+          let regex = new RegExp(value.urlPattern);
+          console.log(sender);
+          return regex.test(sender.tab.url);
+        })
+      );
     } else if (message.request == "set-badge-text") {
-      chrome.browserAction.setBadgeBackgroundColor({ color: "#F00" }, function() {});
-      chrome.browserAction.setBadgeText({ tabId: sender.tab.id, text: message.text.toString() }, function() {});
+      chrome.browserAction.setBadgeBackgroundColor(
+        { color: "#5cb551" },
+        function () {}
+      );
+      chrome.browserAction.setBadgeText(
+        { tabId: sender.tab.id, text: message.text.toString() },
+        function () {}
+      );
     } else {
       sendResponse(null);
     }
--- content.js
+++ content.js
@@ -1,8 +1,46 @@
-window.onload = function() {
-  let rules = [];
-  chrome.runtime.sendMessage({request: "get-rules"}, function(response) {
+let count = 0;
+let rules = [];
+
+window.onload = function () {
+  chrome.runtime.sendMessage({ request: "get-rules" }, function (response) {
     rules = response;
-    hideElements(rules);
+    //highlightElements();
+    hideElements();
+  });
+
+  // Create an observer instance
+  let observer = new MutationObserver(function (mutations) {
+    //console.log(mutations);
+    mutations.forEach(function (mutation) {
+      var newNodes = mutation.addedNodes; // DOM NodeList
+      if (newNodes !== null) {
+        //console.log(newNodes);
+        for (let newNode of newNodes) {
+          console.log(newNode);
+          hideElements(newNode);
+          /*if (mustHide(newNode)) {
+            hideElement(newNode);
+            count++;
+          }*/
+        }
+        // If there are new nodes added
+        /*var $nodes = $(newNodes); // jQuery set
+        $nodes.each(function () {
+          var $node = $(this);
+          if ($node.hasClass("message")) {
+            // do something
+          }
+        });*/
+      }
+    });
+  });
+
+  // Pass in the target node, as well as the observer options
+  observer.observe(document.body, {
+    attributes: false,
+    childList: true,
+    subtree: true,
+    characterData: true,
   });
 
   /*chrome.runtime.onMessage.addListener(function messageReceived(message, sender, sendResponse) {
@@ -14,27 +52,50 @@ window.onload = function() {
   });*/
 };
 
-function hideElements(rules) {
-  let count = 0;
-  console.log(rules);
+function highlightElements() {
+  let lastHighlightedElement = null;
+  document.addEventListener("mouseover", function (e) {
+    e.stopPropagation();
+    let element = e.target;
+    if (lastHighlightedElement !== element) {
+      //console.log(lastHighlightedElement, element);
+      if (lastHighlightedElement !== null) {
+        unhighlightElement(lastHighlightedElement);
+      }
+      highlightElement(element);
+      lastHighlightedElement = element;
+    }
+  });
+}
+
+function highlightElement(element) {
+  element.style.outline = "5px solid red";
+}
+
+function unhighlightElement(element) {
+  element.style.outline = "";
+}
+
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY,
+  };
+}
+
+function hideElements(mainElement = document.body) {
   for (let rule of rules) {
-    let elementsToCheck = document.querySelectorAll(rule.elementSelector);
-    console.log(elementsToCheck);
-    if (elementsToCheck.length > 0) {
-      for (let element of elementsToCheck) {
-        console.log(element.innerHTML);
-        let targetsToCheck = element.querySelectorAll(rule.targetSelector);
-        if (targetsToCheck.length > 0) {
-          for (let target of targetsToCheck) {
-            console.log(target.innerHTML);
-            for (let pattern of rule.contentPatterns) {
-              if (target.innerHTML.toLowerCase().indexOf(pattern.toLowerCase()) >= 0) {
-                count++;
-                element.style.display = "none";
-              }
-            }
-          }
-        }
+    let elementsToCheck = [];
+    if (mainElement.matches(rule.elementSelector)) {
+      elementsToCheck = [mainElement];
+    } else {
+      elementsToCheck = mainElement.querySelectorAll(rule.elementSelector);
+    }
+    for (let element of elementsToCheck) {
+      if (mustHide(element)) {
+        hideElement(element);
+        count++;
       }
     }
   }
@@ -42,6 +103,34 @@ function hideElements(rules) {
     console.log("No element is hidden by Hidem!");
   } else {
     console.log(count + " element(s) are hidden by Hidem!");
-    chrome.runtime.sendMessage({request: "set-badge-text", text: count}, function(response) { });
+    chrome.runtime.sendMessage(
+      { request: "set-badge-text", text: count },
+      function (response) {}
+    );
   }
+}
+
+function hideElement(element) {
+  //console.log(element.innerText);
+  element.style.display = "none";
+  element.classList.add("hidem");
+}
+
+function mustHide(element) {
+  for (let rule of rules) {
+    if (!element.matches(rule.elementSelector)) {
+      return false;
+    }
+    for (let pattern of rule.elementPatterns) {
+      let innerElementsToCheck = element.querySelectorAll(
+        pattern.targetInnerElementSelector
+      );
+      for (let innerElement of innerElementsToCheck) {
+        if (new RegExp(pattern.pattern, "i").test(innerElement.innerText)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
